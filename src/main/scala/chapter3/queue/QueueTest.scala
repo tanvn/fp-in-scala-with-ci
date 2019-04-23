8b6100e601fa5c4e886e198f1d089bfeaafcfcd0
Vu Tan <vunhattan84@gmail.com>
2019-04-24 00:00:36 +0900

    Auto merge (#8)
    * add comment
    
    * add setting

--- src/main/scala/chapter3/queue/QueueTest.scala
+++ src/main/scala/chapter3/queue/QueueTest.scala
@@ -11,6 +11,7 @@ object QueueTest {
     val add30 = add20.enQueue(30)
     println(add10)
 
+    // should test more ??
     add20.head match {
       case None        => println("None")
       case Some(value) => println(s"head is $value")
