bb353471c1a83084504c4c5f2fb1637abb47c58b
Tan Vu <tan.vu@bizreach.co.jp>
2019-04-25 19:26:01 +0900

    add new comment !!!

--- src/main/scala/chapter4/Person.scala
+++ src/main/scala/chapter4/Person.scala
@@ -15,7 +15,7 @@ object Person {
 
   def mkPerson(name: String, age: Int): Either[String, Person] =
     mkName(name).map2(mkAge(age))(Person(_, _))
-  
+
   def main(args: Array[String]): Unit = {
     println(mkPerson("David", 24))
 
