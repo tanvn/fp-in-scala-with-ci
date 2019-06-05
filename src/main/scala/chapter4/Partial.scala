05df03fdbf2390d822410cdb77d29243c5cd1fb1
Tan Vu <tan.vu@bizreach.co.jp>
2019-06-05 19:36:26 +0900

    small commit 2

--- src/main/scala/chapter4/Partial.scala
+++ src/main/scala/chapter4/Partial.scala
@@ -33,7 +33,7 @@ object Partial {
 
     val testData1 = List(19,29,39,40,41, 60, 32)
 
-    
+
     //test traverse
     println(traverse(testData1)(a => if(a % 2 == 0) Success(a/2) else Errors(Seq(s" $a is not an even number"))))
   }
